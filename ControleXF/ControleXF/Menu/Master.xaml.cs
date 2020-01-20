f26c5e15ba270328475dd3bc5f8382f39c26b820
Diogo Freitas <diogo.freitas@hotmail.com>
2020-01-20 20:21:32 -0300

    Adicionar arquivos de projeto.

---
+++ ControleXF/ControleXF/Menu/Master.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControleXF.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void GoActivityIndicatorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ActivityIndicatorPage();
+        }
+    }
+}
